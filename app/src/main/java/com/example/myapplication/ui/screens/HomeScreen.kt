c865a8af66ccc87a408d5e1eb40b7a9aff2e0990
gerardoelcrack2 <conejillolopz@gmail.com>
2024-09-16 21:40:45 -0600

    Added routing to app. Created Home and Menu screen.

---
+++ app/src/main/java/com/example/myapplication/ui/screens/HomeScreen.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication.ui.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun HomeScreen(navController: NavController){
+    Column {
+        Text(text = "This is the Home Screen")
+    }
+}

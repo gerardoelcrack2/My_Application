1c4a3d2fa69dc3f9c58f2393bb6cb93de0c75f06
gerardoelcrack2 <conejillolopz@gmail.com>
2024-11-19 00:04:03 -0600

    Minor fixes

--- app/src/main/java/com/example/myapplication/data/model/MenuModel.kt
+++ app/src/main/java/com/example/myapplication/data/model/MenuModel.kt
@@ -1,7 +1,6 @@
 package com.example.myapplication.data.model
 
 import androidx.compose.ui.graphics.vector.ImageVector
-import java.io.StringBufferInputStream
 
 data class MenuModel(
     val id: Int,

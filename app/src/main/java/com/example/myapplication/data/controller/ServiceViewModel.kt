19d154f6adbce1d2bca200369aba0b9340cb20c7
gerardoelcrack2 <conejillolopz@gmail.com>
2024-11-30 22:03:53 -0600

    Feat: ✨ Added delete function

--- app/src/main/java/com/example/myapplication/data/controller/ServiceViewModel.kt
+++ app/src/main/java/com/example/myapplication/data/controller/ServiceViewModel.kt
@@ -1,20 +1,37 @@
 package com.example.myapplication.data.controller
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.myapplication.data.database.AppDatabase
 import com.example.myapplication.data.model.ServiceModel
+import com.example.myapplication.data.model.toServiceEntityList
 import com.example.myapplication.data.network.RetrofitClient
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import retrofit2.Response
 
 class ServiceViewModel : ViewModel() {
     val api = RetrofitClient.api
 
-    fun getServices(onResult: (Response<List<ServiceModel>>) -> Unit) {
+    fun getServices(db: AppDatabase) {
+        val serviceDao = db.serviceDao()
         viewModelScope.launch {
             try {
                 val response = api.getServices()
-                onResult(response)
+                if (response.body()?.count()!! > 0) {
+                    val serviceEntities = response.body()?.toServiceEntityList()
+                    if (serviceEntities != null) {
+                        CoroutineScope(Dispatchers.IO).launch {
+                            try {
+                                serviceDao.insertAll(serviceEntities)
+                            } catch (exception: Exception) {
+                                Log.d("error", exception.toString())
+                            }
+                        }
+                    }
+                }
             } catch (exception: Exception) {
                 print(exception)
             }
--- app/src/main/java/com/example/myapplication/ui/screens/ManageServiceScreen.kt
+++ app/src/main/java/com/example/myapplication/ui/screens/ManageServiceScreen.kt
@@ -32,8 +32,14 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.example.myapplication.R
 import com.example.myapplication.data.controller.ServiceViewModel
+import com.example.myapplication.data.dao.ServiceDao
+import com.example.myapplication.data.database.AppDatabase
+import com.example.myapplication.data.database.DatabaseProvider
 import com.example.myapplication.data.model.ServiceModel
 import com.example.myapplication.ui.components.TopBar
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -42,8 +48,11 @@ fun ManageServiceScreen(
     serviceId: String?, viewModel:
     ServiceViewModel = viewModel()
 ) {
-    val service = remember { mutableStateOf(ServiceModel()) }
+    val db: AppDatabase = DatabaseProvider.getDatabase(LocalContext.current)
+    val serviceDao = db.serviceDao()
     val context = LocalContext.current
+
+    val service = remember { mutableStateOf(ServiceModel()) }
     var bar_title by remember { mutableStateOf("Create new Service") }
 
     if (serviceId != null && serviceId != "0") {
@@ -197,7 +206,8 @@ fun ManageServiceScreen(
                             viewModel,
                             context,
                             serviceId,
-                            navController
+                            navController,
+                            serviceDao
                         )
                     }) { Text("DELETE") }
             }
@@ -232,11 +242,16 @@ fun delete(
     viewModel: ServiceViewModel,
     context: Context,
     serviceId: String?,
-    navController: NavController
+    navController: NavController,
+    serviceDao: ServiceDao
 ) {
     if (serviceId != null && serviceId != "0") {
         viewModel.deleteService(serviceId.toInt()) { response ->
             if (response.isSuccessful) {
+                CoroutineScope(Dispatchers.IO).launch {
+                    val service = serviceDao.show(serviceId.toInt())
+                    serviceDao.delete(service)
+                }
                 Toast.makeText(context, "Service deleted successfully", Toast.LENGTH_SHORT)
                     .show()
                 navController.popBackStack()
